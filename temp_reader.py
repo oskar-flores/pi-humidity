288310669476df208483d5a3c67d16a2fb37b306
Oskar Flores <ronin99@gmail.com>
2017-01-15 08:53:52 +0100

    added setup function, Rpi configuration

--- temp_reader.py
+++ temp_reader.py
@@ -1,6 +1,8 @@
 import statsd
 import os
 import psutil
+import RPi.GPIO as GPIO
+
 
 host = os.uname()[1]
 cpu = psutil.cpu_percent(interval=1)
@@ -8,3 +10,17 @@ cpu = psutil.cpu_percent(interval=1)
 client = statsd.StatsClient('statsd', 8125, host)
 
 client.gauge('cpu.percent', cpu)
+
+
+def setup_pin_40():
+    GPIO.setmode(GPIO.board)
+    GPIO.setup(40, GPIO.OUT)
+    GPIO.output(40, GPIO.HIGH)
+
+
+def send_humidity(stadsclient, humidity):
+    stadsclient.gauge('lonja.humidity', humidity)
+
+
+def send_temperature(stadsclient, temperature):
+    stadsclient.gauge('lonja.temperature',temperature)
